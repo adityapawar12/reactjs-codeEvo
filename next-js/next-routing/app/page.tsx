045dd3509b57fda22a3c41b8bb89652f47a6d5f9
Aditya <adityapawar743.ap5@gmail.com>
2023-05-27 15:30:10 +0530

    Next js tutorial series by code evolution and completed 5 parts.

---
+++ next-js/next-routing/app/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <h1 className={`font-bold text-4xl`}>Home Page</h1>;
+};
+
+export default Home;
---
+++ next-js/next-routing/app/profile/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Profile = () => {
+  return <h1 className={`font-bold text-4xl`}>Profile Page</h1>;
+};
+
+export default Profile;
